86d3dadce89b969a45b5f416cf4cb18a795a025f
Johannes Elmnäinen <johannes@elmnainen.fi>
2016-05-05 17:11:01 +0300

    fix store creation

--- src/store.js
+++ src/store.js
@@ -4,7 +4,7 @@ import { fromJS, Map } from 'immutable';
 
 import reducer from './reducers/index';
 
-const store = applyMiddleware(thunk)(createStore);
+const store = createStore(reducer, fromJS({}), applyMiddleware(thunk));
 
 if (module.hot) {
   module.hot.accept('./reducers/index', () => {
