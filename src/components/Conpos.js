10e89fe690a9e5ae294e5e72c1f04641c1af58e0
Johannes Elmnäinen <johannes.elmnainen@helsinki.fi>
2016-05-06 23:31:14 +0300

    Componetisize layout

---
+++ src/components/Conpos.js
@@ -0,0 +1,76 @@
+import React, { Component } from 'react';
+
+export default class Compos extends Component {
+
+  render() {
+    return(
+      <div className="content compos">
+        <div className="content-wrapper">
+          <h2>KOMPOT</h2>
+          <div className="compo-boxes">
+
+            <div className="compo-box">
+              <div className="compo-cover" style={{backgroundImage: 'url(http://www.fillmurray.com/g/600/400)'}}>
+              </div>
+              <div className="compo-content">
+                <h4>pe-su</h4>
+                <h3>DOTA 2</h3>
+              </div>
+              <div className="compo-footer">
+                <a href="#">LUE LISÄÄ</a>
+              </div>
+            </div>
+
+            <div className="compo-box">
+              <div className="compo-cover" style={{backgroundImage: 'url(http://www.fillmurray.com/g/600/400)'}}>
+              </div>
+              <div className="compo-content">
+                <h4>pe-su</h4>
+                <h3>DOTA 2</h3>
+              </div>
+              <div className="compo-footer">
+                <a href="#">LUE LISÄÄ</a>
+              </div>
+            </div>
+
+            <div className="compo-box">
+              <div className="compo-cover" style={{backgroundImage: 'url(http://www.fillmurray.com/g/600/400)'}}>
+              </div>
+              <div className="compo-content">
+                <h4>pe-su</h4>
+                <h3>DOTA 2</h3>
+              </div>
+              <div className="compo-footer">
+                <a href="#">LUE LISÄÄ</a>
+              </div>
+            </div>
+
+            <div className="compo-box">
+              <div className="compo-cover" style={{backgroundImage: 'url(http://www.fillmurray.com/g/600/400)'}}>
+              </div>
+              <div className="compo-content">
+                <h4>pe-su</h4>
+                <h3>DOTA 2</h3>
+              </div>
+              <div className="compo-footer">
+                <a href="#">LUE LISÄÄ</a>
+              </div>
+            </div>
+
+            <div className="compo-box">
+              <div className="compo-cover" style={{backgroundImage: 'url(http://www.fillmurray.com/g/600/400)'}}>
+              </div>
+              <div className="compo-content">
+                <h4>pe-su</h4>
+                <h3>DOTA 2</h3>
+              </div>
+              <div className="compo-footer">
+                <a href="#">LUE LISÄÄ</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
---
+++ src/components/Partners.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export default class Partners extends Component {
+
+  render() {
+    return(
+      <div class="content partners">
+        <div class="content-wrapper">
+          <h2>YHTEISTYÖTAHOT</h2>
+        </div>
+      </div>
+    );
+  }
+}
