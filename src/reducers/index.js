91c777d284e6591fd07475456502467677277eb6
Johannes Elmnäinen <johannes@elmnainen.fi>
2016-06-05 13:22:48 +0300

    working landing

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,13 +1,11 @@
 import { combineReducers } from 'redux-immutablejs';
 
-import navigation from './navigationReducer';
 import ingress from './ingressReducer';
 import newsfeed from './newsfeedReducer';
 import compos from './composReducer';
 import partners from './partnersReducer';
 
 const finalReducer = combineReducers({
-  navigation,
   ingress,
   newsfeed,
   compos,
